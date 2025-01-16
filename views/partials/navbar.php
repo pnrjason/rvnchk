0175a0878501471f6a794d8a51c0b28e2ffd94fa
Jason Te <pnrjason@gmail.com>
2025-01-17 01:01:01 +0800

    removed fixed-top

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+<nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center"></div>
     <div class="navbar-menu-wrapper d-flex align-items-stretch">
         <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
